0e874cb1630801811918784ee1669780efeb6e62
AutumnStarz <143517148+AutumnStarz@users.noreply.github.com>
2025-02-04 10:18:32 -0500

    commit feb 4

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,39 +1,115 @@
 
-
-const respondJSON = (request, response, status, object) => {
-  const content = JSON.stringify(object);
-  const headers = {
-    'Content-Type': 'application/json',
-    'Content-Length': Buffer.byteLength(content, 'utf8'),
-  };
-
-  response.writeHead(status, headers);
-  if(request.method !== 'HEAD') {
-    response.write(content);
+//XML conversion function (i hope this is right i hate xml formatting??)
+const convertObjectToXML = (object, status) => {
+  let xml = '\n<response>';
+  if (status === 200) {
+    xml += `<message>${object.message}</message>`; //add message to xml
+  } else {
+    xml += `<message>${object.message}</message>`; //add message to xml
+    xml += `<id>${object.id}</id>`; //add id to xml
   }
+  xml += '</response>';
+  return xml;
+};
 
+const respond = (request, response, status, object) => { //function to respond to requests
+  let responseContent = '';
+  let contentType = 'application/json';
+  if (request.headers.accept && request.headers.accept.includes('text/xml')) {
+    contentType = 'text/xml';
+    responseContent = convertObjectToXML(object, status);
+  } else {
+    responseContent = JSON.stringify(object);
+  }
+  console.log(responseContent);
+  const headers = {
+    'Content-Type': contentType,
+    'Content-Length': Buffer.byteLength(responseContent, 'utf8'),
+  };
+  response.writeHead(status, headers);
+  if (request.method !== 'HEAD') {
+    response.write(responseContent);
+  }
   response.end();
 };
 
-const notFound = (request, response) => {
-  const responseJSON = {
+const getSuccess = (request, response) => { //success response
+  const responseObj = {
+    message: 'This is a successful response.',
+    id: 'success',
+  };
+  return respond(request, response, 200, responseObj);
+};
+
+const getBadRequest = (request, response) => { //bad request response
+  const parsedUrl = new URL(request.url, `http://${request.headers.host}`);
+  if (parsedUrl.searchParams.get('valid') === 'true') {
+    const responseObj = {
+      message: 'Missing valid query paramter.',
+    };
+    return respond(request, response, 200, responseObj);
+  }
+  const responseObj = {
+    message: 'Missing valid query parameter set to true.',
+    id: 'badRequest',
+  };
+  return respond(request, response, 400, responseObj);
+};
+
+const getUnauthorized = (request, response) => { //unauthorized response
+  const parsedUrl = new URL(request.url, `http://${request.headers.host}`);
+  if (parsedUrl.searchParams.get('loggedIn') === 'yes') {
+    const responseObj = {
+      message: 'You have successfully viewed the content.',
+    };
+    return respond(request, response, 200, responseObj);
+  }
+  const responseObj = {
+    message: 'Missing loggedIn query parameter set to yes.',
+    id: 'unauthorized',
+  };
+  return respond(request, response, 401, responseObj);
+};
+
+const getForbidden = (request, response) => { //forbidden response
+  const responseObj = {
+    message: 'You do not have access to this content.',
+    id: 'forbidden',
+  };
+  return respond(request, response, 403, responseObj);
+};
+
+const getInternal = (request, response) => { //internal server error response (aka server exploded)
+  const responseObj = {
+    message: 'Internal Server Error. Something went wrong.',
+    id: 'internal',
+  };
+  return respond(request, response, 500, responseObj);
+};
+
+const getNotImplemented = (request, response) => { //not implemented response
+  const responseObj = {
+    message:
+      'A GET request for this page has not been implemented yet. Check again later for updated content.',
+    id: 'notImplemented',
+  };
+  return respond(request, response, 501, responseObj);
+};
+
+const notFound = (request, response) => { //not found response
+  const responseObj = {
     message: 'The page you are looking for was not found.',
     id: 'notFound',
-  }
-
-  return respondJSON(request, response, 404, responseJSON);
+  };
+  return respond(request, response, 404, responseObj);
 };
 
-const getSuccess = (request, response) => {
-    const responseJSON = {
-        message: 'This is a successful response.',
-        id: 'success',
-    }
-    
-    return respondJSON(request, response, 200, responseJSON);
-};
-
-module.exports = {
-  notFound,
+module.exports = { //export functions
   getSuccess,
+  getBadRequest,
+  getUnauthorized,
+  getForbidden,
+  getInternal,
+  getNotImplemented,
+  notFound,
 };
--- src/server.js
+++ src/server.js
@@ -4,18 +4,22 @@ const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const urlStruct = {
+const urlStruct = { //url structure with endpoints
   '/': htmlHandler.getIndex,
   '/style.css': htmlHandler.getCSS,
-  '/success': jsonHandler.success, //??
+  '/success': jsonHandler.getSuccess,
+  '/badRequest': jsonHandler.getBadRequest,
+  '/unauthorized': jsonHandler.getUnauthorized,
+  '/forbidden': jsonHandler.getForbidden,
+  '/internal': jsonHandler.getInternal,
+  '/notImplemented': jsonHandler.getNotImplemented,
   notFound: jsonHandler.notFound,
-}
+};
 
 const onRequest = (request, response) => {
   const protocol = request.connection.encrypted ? 'https' : 'http';
   const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
-
-  if(urlStruct[parsedUrl.pathname]){
+  if (urlStruct[parsedUrl.pathname]) {
     urlStruct[parsedUrl.pathname](request, response);
   } else {
     urlStruct.notFound(request, response);
